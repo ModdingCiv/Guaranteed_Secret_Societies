7f050bbe70d0b46f5d5217eb3f37c894a6956fc7
Kevin van Rijn <kevinvanrijn@users.noreply.github.com>
2020-08-08 15:57:08 +0200

    Initial commit

---
+++ Guaranteed_Secret_Societies.sql
@@ -0,0 +1,4 @@
+UPDATE SecretSocieties SET DiscoverAtNaturalWonderBaseChance=100 WHERE SecretSocietyType='SECRETSOCIETY_HERMETIC_ORDER';
+UPDATE SecretSocieties SET DiscoverAtCityStateBaseChance=100 WHERE SecretSocietyType='SECRETSOCIETY_OWLS_OF_MINERVA';
+UPDATE SecretSocieties SET DiscoverAtBarbarianCampBaseChance=100 WHERE SecretSocietyType='SECRETSOCIETY_SANGUINE_PACT';
+UPDATE SecretSocieties SET DiscoverAtGoodyHutBaseChance=100 WHERE SecretSocietyType='SECRETSOCIETY_VOIDSINGERS';
